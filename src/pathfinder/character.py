1fb38d2b5dee913ad86f09593d8c1f7643f6002a
masoncowen <mason.owen36@gmail.com>
2024-06-17 23:28:10 +0100

    Added enum of classes and separated temporary character variables into status

--- src/pathfinder/character.py
+++ src/pathfinder/character.py
@@ -1,4 +1,41 @@
+import pydantic
+
+from enum import Enum, auto
+from typing import Optional
+
+# This class is for temporary information
+class Status(pydantic.BaseModel):
+  initiative: int
+  actions_remaining: int = 3
+
+class pfClass(Enum):
+  Alchemist = auto()
+  Barbarian = auto()
+  Bard = auto()
+  Champion = auto()
+  Cleric = auto()
+  Druid = auto()
+  Fighter = auto()
+  Gunslinger = auto()
+  Inventor = auto()
+  Investigator = auto()
+  Kineticist = auto()
+  Magus = auto()
+  Monk = auto()
+  Oracle = auto()
+  Psychic = auto()
+  Ranger = auto()
+  Rogue = auto()
+  Sorcerer = auto()
+  Summoner = auto()
+  Swashbuckler = auto()
+  Thaumaturge = auto()
+  Witch = auto()
+  Wizard = auto()
+
 class Character(pydantic.BaseModel):
   name: str
-  actions_remaining: int = 3
-  max_actions: int = 3
+  level: int
+  AC: int #TODO: calculate AC = 10 + min(DEX, Armor.Dex_cap) + proficiency bonus (level + 2,4,6,8) + armour's item bonus + other bonuses + penalties
+  pf_class: pfClass
+  status: Optional[Status] = None
