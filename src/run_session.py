3408196795a6b20c6e16654a6c5c885aa85ca796
masoncowen <mason.owen36@gmail.com>
2024-06-25 00:28:09 +0100

    Not currently working, oops. But separated function from command (now CommandInfo)

--- src/run_session.py
+++ src/run_session.py
@@ -15,9 +15,28 @@ class States(Enum):
   CombatSetup = auto()
   Combat = auto()
 
+class CommandInfo(pydantic.BaseModel):
+  text: str
+  description: str
+  allowed_states: Optional[set[States]] = None
+  blocked_states: Optional[set[States]] = None
+  arguments: Optional[Arguments] = None
+
+class CommandList(Enum):
+  HELP = CommandInfo(text = 'HELP', description = "Prints this message with no arguments or provides more detail on another command.")
+  TODO = CommandInfo(text = 'TODO', description = "Appends note to todo list.")
+  QUIT = CommandInfo(text = 'QUIT', description = "Ignores any arguments, quits program.")
+  ECHO = CommandInfo(text = 'ECHO', description = "Echoes arguments.")
+  BREAK = CommandInfo(text = 'BREAK', description = "Desginates start or end of a break, must be followed by 'start' or 'stop'.")
+  COMBAT = CommandInfo(text = 'COMBAT', description = "Begins a combat encounter.", blocked_states = {States.Break})
+  SAVE = CommandInfo(text = 'SAVE', description = "Saves current context to file to load later. Program loads most recent save.")
+  LOAD = CommandInfo(text = 'LOAD', description = "Loads different saved context if most recent save isn't desired.")
+
 class Context(pydantic.BaseModel):
   state: States
-  character: Optional[Character] = None
+  engine: Engine
+  command_info: Optional[CommandInfo] = None
+  arguments: Arguments
   previous_context: Optional[Self] = None
 
   def command_prompt(self: Self) -> str:
@@ -32,145 +51,126 @@ class Context(pydantic.BaseModel):
                                     self.character.max_actions)
     return 'ERROR: '
 
-class Command(pydantic.BaseModel):
-  text: str
-  description: str
-  function: Callable[[Context, Arguments], Context]
-  allowed_states: Optional[set[States]] = None
-  blocked_states: Optional[set[States]] = None
+  def get_command(self: Self) -> None:
+    command_str: str = input(context.command_prompt())
+    command_text: str = command_str.split(' ')[0]
+    arguments: Arguments = command_str.split(' ')[1:]
+    try:
+      self.command_info: CommandInfo =  CommandList[command_text.upper()].value
+      self.command_info.arguments = arguments
+    except KeyError:
+      log.warning("Unrecognised command.")
 
-  def can_run_in_context(self: Self, context: Context) -> bool:
-    if self.blocked_states is not None:
-      if context.state in self.blocked_states:
-        log.warning("Command is not allowed in current state: ({}, {}).".format(self.text, context.state))
+  def main_loop(self: Self) -> None:
+    self.get_command()
+    if self.command_info is None:
+      return None
+    log.debug("Command identified as: {}".format(self.command_info))
+    if not self.can_run_command():
+      return None
+    log.debug("Command is runnable in this context")
+    self.write_generic_history()
+    self.engine.run_command(self.command_info)
+    return None
+
+  def can_run_command(self: Self) -> bool:
+    if self.command_info.blocked_states is not None:
+      if self.state in self.command_info.blocked_states:
+        log.warning("Command is not allowed in current state: ({}, {}).".format(self.command_info.text, self.state))
         return False
-    if self.allowed_states is not None:
-      if context.state not in self.allowed_states:
-        log.warning("Command is not allowed in current state: ({}, {}).".format(self.text, context.state))
+    if self.command_info.allowed_states is not None:
+      if self.state not in self.command_info.allowed_states:
+        log.warning("Command is not allowed in current state: ({}, {}).".format(self.command_info.text, self.state))
         return False
     return True
 
-  def write_generic_history(self: Self, args: Arguments) -> None:
-    hist_line = "> Ran '{}' with {}.".format(self.text.lower(), args)
+  def write_generic_history(self: Self) -> None:
+    hist_line = "> Ran '{}' with {}.".format(self.command_info.text.lower(), self.args)
     log.history(hist_line)
 
-def printHelp(context: Context, args: Arguments) -> Context:
-  if args == [] or args[0].upper() == Commands.HELP.value.text:
-    log.info("Following commands:")
-    for command_container in Commands:
-      command = command_container.value
-      log.info("'{}' - {}".format(command.text.lower(), command.description))
-  elif len(args) == 1:
-    log.info("Specific help is not yet supported.")
-  return context
+  def printHelp(self: Self) -> None:
+    if self.args == [] or self.args[0].upper() == CommandList.HELP.value.text:
+      log.info("Following command:")
+      for command_container in CommandList:
+        command_info = command_container.value
+        log.info("'{}' - {}".format(self.command_info.text.lower(), self.command_info.description))
+    elif len(self.args) == 1:
+      log.info("Specific help is not yet supported.")
+    return
 
-def addTodo(context: Context, args: Arguments) -> Context:
-  todo = " ".join(args)
-  with open('../todolist.md', 'a') as todo_list:
-    todo_list.write('- [ ] {}'.format(todo))
-  log.info("Added '{}' to todo list.".format(todo))
-  return context
+  def addTodo(self: Self) -> None:
+    todo = " ".join(self.args)
+    with open('../todolist.md', 'a') as todo_list:
+      todo_list.write('- [ ] {}'.format(todo))
+    log.info("Added '{}' to todo list.".format(todo))
+    return None
 
-def quitSession(context: Context, args: Arguments) -> Context:
-  quit()
-  return context
+  def quitSession(self: Self) -> None:
+    quit()
+    return None
 
-def echoArguments(context: Context, args: Arguments) -> Context:
-  log.info(args)
-  return context
+  def echoArguments(self: Self) -> None:
+    log.info(self.args)
+    return None
 
-def controlBreak(context: Context, args: Arguments) -> Context:
-  if len(args) == 0:
-    log.warning("Command must be followed by either 'start' or 'stop'")
-    return context
-  elif len(args) > 1:
-    log.warning("Too many arguments")
-    return context
-  sub_command = args[0]
-  match sub_command.upper():
-    case "START":
-      log.info("Beginning break")
-      return Context(state = States.Break,
-                     previous_context = context)
-    case "STOP":
-      log.info("Resuming session")
-      return context.previous_context
-    case _:
+  def controlBreak(self: Self) -> None:
+    if len(self.args) == 0:
       log.warning("Command must be followed by either 'start' or 'stop'")
-      return context
+      return None
+    elif len(args) > 1:
+      log.warning("Too many arguments")
+      return None
+    sub_command = self.args[0]
+    match sub_command.upper():
+      case "START":
+        log.info("Beginning break")
+        self.saveContext()
+        self.state = States.Break
+      case "STOP":
+        log.info("Resuming session")
+        self.loadContext()
+      case _:
+        log.warning("Command must be followed by either 'start' or 'stop'")
+    return None
 
-def beginCombat(context: Context, args: Arguments) -> Context:
-  log.info("Feature not yet implemented")
-  return context
+  def beginCombat(self: Self) -> None:
+    log.info("Feature not yet implemented")
+    return None
 
-def saveContext(context: Context, args: Arguments) -> Context:
-  log.info("Feature not yet implemented")
-  return context
+  def saveContext(self: Self) -> None:
+    log.info("Feature not yet implemented")
+    return None
 
-def loadContext(context: Context, args: Arguments) -> Context:
-  log.info("Feature not yet implemented")
-  return context
+  def loadContext(self: Self) -> None:
+    log.info("Feature not yet implemented")
+    return None
+  
+class Command(pydantic.BaseModel):
+  function: Callable[[Context, Arguments], Context]
 
-class Commands(Enum):
-  HELP = Command(text = 'HELP',
-                 description = "Prints this message with no arguments or provides more detail on another command.",
-                 function = printHelp)
-  TODO = Command(text = 'TODO',
-                 description = "Appends note to todo list.",
-                 function = addTodo)
-  QUIT = Command(text = 'QUIT',
-                 description = "Ignores any argumments, quits program.",
-                 function = quitSession)
-  ECHO = Command(text = 'ECHO',
-                 description = "Echoes arguments.",
-                 function = echoArguments)
-  BREAK = Command(text = 'BREAK',
-                  description = "Desginates start or end of a break, must be followed by 'start' or 'stop'.",
-                  function = controlBreak)
-  COMBAT = Command(text = 'COMBAT',
-                   description = "Begins a combat encounter.",
-                   function = beginCombat,
-                   blocked_states = {States.Break})
-  SAVE = Command(text = 'SAVE',
-                 description = "Saves current context to file to load later. Program loads most recent save.",
-                 function = saveContext)
-  LOAD = Command(text = 'LOAD',
-                 description = "Loads different saved context if most recent save isn't desired.",
-                 function = loadContext)
+
+
 
 def start_session() -> Context:
   log.info("New session started")
   return Context(state = States.Exploration)
 
-def get_command(context: Context) -> tuple[Optional[Command], Arguments]:
-  command_str: str = input(context.command_prompt())
-  command_text: str = command_str.split(' ')[0]
-  args: tuple[str] = command_str.split(' ')[1:]
-  try:
-    command: Command =  Commands[command_text.upper()].value
-    return (command, args)
-  except KeyError:
-    log.warning("Unrecognised command.")
-    return (None, args)
+def get_command_function(
+                 function = printHelp)
+                 function = addTodo)
+                 function = quitSession)
+                 function = echoArguments)
+                  function = controlBreak)
+                   function = beginCombat,
+                 function = saveContext)
+                 function = loadContext)
 
-def main_loop(context: Context) -> Context:
-  command, args = get_command(context)
-  if command is None:
-    return context
-  log.debug("Command identified as: {}".format(command))
 
-  if not command.can_run_in_context(context):
-    return context
-  log.debug("Command is runnable in this context")
-
-  command.write_generic_history(args)
-  context = command.function(context, args)
-  return context
 
 def main() -> None:
   context = start_session()
   while True:
-    context = main_loop(context)
+    context.main_loop()
 
 if __name__ == '__main__':
   main()
